654155e359090e1bca9600ebf292ad3744a336be
sharadpandey2 <62sharadpandey@gmail.com>
2026-02-23 17:32:13 +0530

    Fix: added use client to AdminDashboard

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+"use client"; 
 import { useState, useRef, useEffect } from "react";
 import { motion, useScroll, useTransform, AnimatePresence } from "framer-motion";
 import { 
